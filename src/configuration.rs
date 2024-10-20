3c6351d02716de54d2a90b30c61ae63166c2c180
Syiin <xbio.oo@gmail.com>
2024-10-20 17:49:49 +0800

    introduce a config yaml file and read from it

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,14 +14,10 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Initialise our configuration reader
     let settings = config::Config::builder()
-        // Add configuration values from a file named `configuration.yaml`.
         .add_source(
-            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+            config::File::new("config.yaml", config::FileFormat::Yaml)
         )
         .build()?;
-    // Try to convert the configuration values it read into
-    // our Settings type
     settings.try_deserialize::<Settings>()
 }
