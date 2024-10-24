213a3c62c12d54d6138255dd5b3cc4b5f31325cd
Syiin <xbio.oo@gmail.com>
2024-10-24 23:01:36 +0800

    change endpoint URL to health-check

--- src/startup.rs
+++ src/startup.rs
@@ -12,7 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
             App::new()
                 .wrap(TracingLogger::default())
-                .route("/health_check", web::get().to(health_check))
+                .route("/health-check", web::get().to(health_check))
                 .route("/subscriptions", web::post().to(subscribe))
                 .app_data(db_pool.clone())
         })
