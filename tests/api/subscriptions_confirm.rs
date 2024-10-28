0fedba32e4392a069f42f7e79a8a8dec5b45e794
Syiin <xbio.oo@gmail.com>
2024-10-28 16:17:22 +0800

    use transactions

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,32 +1,6 @@
 use crate::helpers::spawn_app;
-use reqwest::Url;
-use wiremock::{ResponseTemplate, Mock};
-use wiremock::matchers::{path, method};
-
-#[tokio::test]
-async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
-    // Arrange
-    let app = spawn_app().await;
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
-    Mock::given(path("/email"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        .mount(&app.email_server)
-        .await;
-
-    app.post_subscriptions(body.into()).await;
-    let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
-
-    // Act
-    let response = reqwest::get(confirmation_links.html)
-        .await
-        .unwrap();
-
-    // Assert
-    assert_eq!(response.status().as_u16(), 200);
-}
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn confirmations_without_token_are_rejected_with_a_400() {
@@ -43,6 +17,29 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 }
 
 #[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // Act
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
 async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
     // Arrange
     let app = spawn_app().await;
@@ -56,7 +53,7 @@ async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
 
     // Act
     reqwest::get(confirmation_links.html)
