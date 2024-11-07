75ba7420cf287c0649376f4c56bcba4f781d6670
Syiin <xbio.oo@gmail.com>
2024-11-07 23:56:30 +0800

    naive solution to page rerouting

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -55,10 +55,13 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, "/login"))
+            .finish()
+    }
+
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
+        StatusCode::SEE_OTHER
     }
 }
